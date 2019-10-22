2780e0c5006b985cdbe9297fcf8765a790db5244
Gabriela Shaooli <gshaooli@lcs.on.ca>
2019-10-22 12:54:34 -0400

    Added app icon and trying to get input fields working

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    // (outlets connected to user inteface items)
+    // (variables and constants that we need to use below)
+    @IBOutlet weak var textfieldAmountOfBill: UITextField!
+    
+    @IBOutlet weak var textfieldSplitTipBetweenHowManyPeople: UITextField!
+    @IBOutlet weak var labelTotalTip: UILabel!
+    
+    @IBOutlet weak var TipPerPerson: UILabel!
+    
+    // MARK: Methods
+    // Behaviours the app needs to do
+    
+    // This method runs once when the view appears
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func calculateFivePercentTip(_ sender: Any) {
+        
+        let amountOfBillAsString = textfieldAmountOfBill.text!
+        
+    }
+    
 }
